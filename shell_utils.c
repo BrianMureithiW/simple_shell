2fa7bbf8011a58ee1b9938a334a630f481cc6969
Zachariarian <zachariarian@gmail.com>
2022-06-15 00:40:54 -0700

    betty

--- shell_utils.c
+++ shell_utils.c
@@ -24,7 +24,11 @@ for (i = 0; internal_command[i] != NULL; i++)
 if (_strcmp(command, internal_command[i]) == 0)
 return (INTERNAL_COMMAND);
 }
-/* @check_path - checks if a command is found in the PATH */
+/**
+ * @check_path - checks if a command is
+ * found in the PATH
+ * Return - path command or invalid command
+ */
 path = check_path(command);
 if (path != NULL)
 {
